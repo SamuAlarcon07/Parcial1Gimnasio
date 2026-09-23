66d5586ad9f460dfdfe80d327c5febdf9b8c6fb1
Samuel Giraldo Betancur <samuel.giraldob@uqvirtual.edu.co>
2026-09-23 16:31:22 -0500

    Clase FactoryPlanPersonalizado y FactoryPlanPremium completas

--- src/main/java/factory/FactoryPlanPremium.java
+++ src/main/java/factory/FactoryPlanPremium.java
@@ -1,4 +1,37 @@
 package factory;
 
-public class FactoryPlanPremium {
+
+import co.edu.uniquindio.parcial1gimnasio.model.Plan;
+import co.edu.uniquindio.parcial1gimnasio.model.PlanPremium;
+
+public class FactoryPlanPremium extends FactoryPlan {
+
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private int duracionMeses;
+    private double valorMensual;
+
+    public FactoryPlanPremium(String codigo, String nombre,
+                              String descripcion, int duracionMeses,
+                              double valorMensual) {
+
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.duracionMeses = duracionMeses;
+        this.valorMensual = valorMensual;
+    }
+
+    @Override
+    public Plan crearPlan() {
+
+        return new PlanPremium(
+                codigo,
+                nombre,
+                descripcion,
+                duracionMeses,
+                valorMensual
+        );
+    }
 }
